93a333d16243243b5ecb1bd011b0fb8d1e79e599
Harwood <1222752+Harwood@users.noreply.github.com>
2018-10-25 13:54:58 -0500

    update to 0.1.1

--- YouTube DL/Pythonista/yt-dl.py
+++ YouTube DL/Pythonista/yt-dl.py
@@ -1,30 +1,28 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" yt-dl.py: """
+""" yt-dl.py '{"options": [], "urls": ["https://www.youtube.com/watch?v=BaW_jenozKc"]}' """
 
 import json
 import os
 import sys
-from logzero import logger
 import youtube_dl
 
 __author__ = "Harwood"
 __copyright__ = ""
 __credits__ = ["Harwood"]
 __license__ = ""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __maintainer__ = "Harwood"
 __email__ = ""
 __status__ = "Development"
 
 
-def main(ydl_opts, urls):
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		ydl.download(urls)
+def main(input):
+	# TODO: validate JSON structure of input
+	
+	with youtube_dl.YoutubeDL(input['options']) as ydl:
+		ydl.download(input['urls'].splitlines())
 
 
 if __name__ == '__main__':
-	input = json.loads(sys.argv[1])
-	urls = input['urls'].splitlines()
-
-	sys.exit(main(input['options'], urls))
+	sys.exit(main(json.loads(sys.argv[1])))
