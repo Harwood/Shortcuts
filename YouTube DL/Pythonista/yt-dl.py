181f1b331e480088dffca13e770d981a8189b717
Carter Harwood <1222752+Harwood@users.noreply.github.com>
2018-11-01 12:02:05 -0500

    0.1.2 abstracted to class, added notifications

--- YouTube DL/Pythonista/yt-dl.py
+++ YouTube DL/Pythonista/yt-dl.py
@@ -1,27 +1,55 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" yt-dl.py '{"options": [], "urls": ["https://www.youtube.com/watch?v=BaW_jenozKc"]}' """
+'''
+yt-dl.py '{"options": [], "urls": ["https://www.youtube.com/watch?v=BaW_jenozKc"]}'
+'''
 
 import json
+import notification
 import os
 import sys
+import webbrowser
 import youtube_dl
 
 __author__ = "Harwood"
 __copyright__ = ""
 __credits__ = ["Harwood"]
-__license__ = ""
-__version__ = "0.1.1"
+__license__ = "MIT"
+__version__ = "0.2.0"
 __maintainer__ = "Harwood"
 __email__ = ""
 __status__ = "Development"
 
 
+class Shortcut:
+	def __init__(self, options):
+		# TODO: validate options
+		options['progress_hooks'] = [Shortcut.hook]
+		self.ytdl = youtube_dl.YoutubeDL(options)
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		pass
+
+	def download(self, urls):
+		# TODO: validate urls
+		self.ytdl.download(urls)
+
+		webbrowser.open('shortcuts://')
+
+	def hook(d):
+		if d['status'] == 'finished':
+			title = d['filename'].split('/')[-1].split(' - ', 1)[-1].rsplit(' - ', 1)[0]
+
+			notification.schedule(f'Download of "{title}" completed', 1, 'default', '')
+
+
 def main(input):
 	# TODO: validate JSON structure of input
-	
-	with youtube_dl.YoutubeDL(input['options']) as ydl:
-		ydl.download(input['urls'].splitlines())
+	with Shortcut(input['options']) as shortcut:
+		shortcut.download(input['urls'].splitlines())
 
 
 if __name__ == '__main__':
--- YouTube DL/README.rst
+++ YouTube DL/README.rst
@@ -11,6 +11,14 @@ Utilizes `rg3/youtube-dl`_ for the audio/video downloading from a long list of `
 :code:`On My iPhone/Local Storage/Downloads` will need to be added to Pythonista under External Files.
 
 
+Setup
+-----
+Install StaSH using these in
+https://github.com/ywangd/stash/blob/master/README.md#Installation
+
+
+Running the shortcut while a download is still in progress will queue up the new video to download once the current one completes. Multiple downloads can be queued up like this. 
+
 .. _rg3/youtube-dl: https://github.com/rg3/youtube-dl 
 .. _supported sites: https://github.com/rg3/youtube-dl/blob/master/docs/supportedsites.md
 .. _Project Tracking: https://github.com/Harwood/Shortcuts/projects/1
